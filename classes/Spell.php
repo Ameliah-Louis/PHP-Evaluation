b87e701da3e8ee56df04b26bf298b790e40b3607
Améliah-LOUIS <alouis@stagiaire-humanbooster.com>
2023-08-12 16:58:43 +0200

    del damage.php & bdd creation

--- classes/Spell.php
+++ classes/Spell.php
@@ -1,31 +1,31 @@
 <?php 
 require_once 'Damage.php';
 
-class Spell {
+class Spell 
+{
+    public int $id;
     public string $name;
-    public string $school;
     public int $level;
-    public $damage = 'class Damage';
-    public array $desc;
-    public boolval $ritual;
+    public int $school_id;
+    public string $desc;
 
-    public function __construct($name, $school, $level, Damage $damage, $desc, $ritual) {
-        $this->name = $name;
-        $this->school = $school;
-        $this->level = $level;
-        $this->damage = '???????????';
-        $this->desc = $desc;
-        $this->ritual = $ritual;
+
+
+    public function __construct($id, $name, $level, $school_id, $desc) 
+    {
+    $this->id = $id;
+    $this->name = $name;
+    $this->level = $level;
+    $this->school_id = $school_id;
+    $this->desc = $desc;
     }
 }
     //  $name = "Eldritch Blast"
-    //  $school = classe Evocation with parameters school_name & school_image,
+    //  $school_id = classe Evocation with parameters school_name & school_image,
     //  $level = int 0,
-    //  $damage = classe Damage with parameters /damage_type & damage_type_icon related/ & damage_dice,
     //  $desc = ["A beam of crackling energy streaks toward a creature within range. 
     //                 Make a ranged spell attack against the target. On a hit, the target takes 1d10 force damage. 
     //                 The spell creates more than one beam when you reach higher levels: two beams at 5th level, three beams at 11th level, and four beams at 17th level. 
     //                 You can direct the beams at the same target or at different ones. Make a separate attack roll for each beam."
     //                 ]
-    //  $ritual = false,
     //  $class = [["warlock","Warlock"]]
--- index.php
+++ index.php
@@ -2,3 +2,62 @@
 
 $damage = new Damage();
 $spell = new Spell();
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Liste des sorts</title>
+</head>
+<body>
+<form name="filtres" action="sorts.php" method="post">
+<div class="col1">
+    <div class="select normal">
+    <label><input type="checkbox" id="selectAllF1" checked="checked" onclick="selectF(1)"> Personnage</label> <i title="Ctrl+Click pour sélectionner plusieurs éléments"></i><br>
+        <select name="Filtre1[]" id="FormF1" multiple size="3">
+            <option value="c" selected="selected">Selmays</option>
+            <option value="d" selected="selected">Darkna</option>
+            <option value="w" selected="selected">Leowen</option>
+        </select>
+    </div><div class="select small">Niv Min
+        <select name="nivMin">
+            <option value=0 selected="selected">0</option>
+            <option value=1 >1</option>
+            <option value=2 >2</option>
+            <option value=3 >3</option>
+            <option value=4	>4</option>
+            <option value=5 >5</option>
+            <option value=6 >6</option>
+            <option value=7 >7</option>
+            <option value=8 >8</option>
+            <option value=9	>9</option>
+        </select>
+    </div><div class="select small">Niv Max
+        <select name="nivMax">
+            <option value=0 >0</option>
+            <option value=1 >1</option>
+            <option value=2	>2</option>
+            <option value=3	>3</option>
+            <option value=4 >4</option>
+            <option value=5 >5</option>
+            <option value=6 >6</option>
+            <option value=7	>7</option>
+            <option value=8	>8</option>
+            <option value=9 selected="selected">9</option>
+        </select>
+    </div>
+</div><div class="colupdown">
+    <i class="fas fa-chevron-up" onclick="filterUp()"></i><i class="fas fa-chevron-down" onclick="filterDown()" style="display:none"></i>
+</div><div class="col">
+    <a href="#pied"><i class="fas fa-arrow-circle-down" title="Bas de page"></i></a>
+    <input type="submit" name="filtrer" value="FILTRER">
+    <i class="fas fa-sync-alt" title="Reset" onclick="reset()"></i>
+    <i class="fas fa-search"></i><input id="myfilter" type="text" value="" onkeyup="filter()"><i class="fas fa-times" onclick="clearFilter()"></i>
+</div>
+</form>
+
+    
+</body>
+</html>
